1737728d0bb4834359ffcbff0386792f9ab01a20
amber.li <amber.li@starlight-sms.com>
2025-02-07 15:24:14 +0800

    Add Figma integration extension method
    Introduce a new static class `Extensions` within the `FigmaPareNet` namespace containing an extension method `AddFigmaPareNet` for the `IServiceCollection` interface. This method configures Figma-related services by:
    - Adding configuration settings for `FigmaConfig` from the application's configuration.
    - Throwing an exception if the Figma configuration is missing.
    - Registering `FigmaClient` and `FigmaPareService` as scoped services in the dependency injection container.

---
+++ Extensions.cs
@@ -0,0 +1,27 @@
+﻿using FigmaPareNet.Client;
+using FigmaPareNet.Config;
+using FigmaPareNet.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FigmaPareNet
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddFigmaPareNet(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<FigmaConfig>(configuration.GetSection("Figma"));
+            var config = configuration.GetSection("Figma").Get<FigmaConfig>();
+
+            if (config == null)
+            {
+                throw new Exception("Figma configuration is missing");
+            }
+
+            services.AddScoped<IFigmaClient, FigmaClient>();
+            services.AddScoped<IFigmaPareService, FigmaPareService>();
+
+            return services;
+        }
+    }
+}
